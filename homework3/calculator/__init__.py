600b8535afb0eaa465002666a74ea700a631b722
Anneryshc <afh9@njit.edu>
2024-02-14 23:37:31 -0500

    part2 complete

--- homework3/calculator/__init__.py
+++ homework3/calculator/__init__.py
@@ -1,9 +1,20 @@
-"""This is a module containing calculation functions."""
+from calculator.calculation import Calculation
+from calculator.operations import add, subtract, multiply, divide
 
-def add(argument_a, argument_b):
-    """This function returns the sum of two numbers."""
-    return argument_a + argument_b
-
-def subtract(argument_a, argument_b):
-    """This function returns the difference of two numbers."""
-    return argument_a - argument_b
+class Calculator:
+    @staticmethod
+    def add(a,b):
+        calculation = Calculation(a, b, add)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def subtract(a,b):
+        calculation = Calculation(a, b, subtract)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def multiply (a,b):
+        calculation = Calculation(a, b, multiply)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def divide(a,b):
+        calculation = Calculation(a, b, divide)  # Pass the add function from calculator.operations
+        return calculation.get_result()
